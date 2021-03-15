2e5c6e8347372e66cdde266725ac042eba7d2fd0
Phill Hallam-Baker <phill@hallambaker.com>
2021-03-15 18:13:04 -0400

    Initial schema drafts

--- Specification/Exceptions.cs
+++ Specification/Exceptions.cs
@@ -1,5 +1,5 @@
 ﻿
-//  This file was automatically generated at 3/15/2021 1:22:12 PM
+//  This file was automatically generated at 3/15/2021 6:08:39 PM
 //   
 //  Changes to this file may be overwritten without warning
 //  

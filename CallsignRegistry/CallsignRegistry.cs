9f12888f1af4a08813a270714334238c73452f5d
Phill Hallam-Baker <phill@hallambaker.com>
2021-03-15 15:20:23 -0400

    Initial repository creation.

---
+++ CallsignRegistry/CallsignRegistry.cs
@@ -0,0 +1,101 @@
+﻿
+//  Copyright (c) 2016 by .
+//  
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//  
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//  
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//  
+//  
+//  This file was automatically generated at 3/15/2021 1:22:08 PM
+//   
+//  Changes to this file may be overwritten without warning
+//  
+//  Generator:  protogen version 3.0.0.566
+//      Goedel Script Version : 0.1   Generated 
+//      Goedel Schema Version : 0.1   Generated
+//  
+//      Copyright : © 2015-2019
+//  
+//  Build Platform: Win32NT 10.0.18362.0
+//  
+//  
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+using Goedel.Protocol;
+
+
+#pragma warning disable IDE1006
+
+
+using Goedel.Cryptography.Jose;
+using Goedel.Cryptography.Dare;
+
+
+namespace Goedel.Callsign.Registry {
+
+
+	/// <summary>
+	///
+	/// Callsign Registry Protocol providing registry interactions.
+	/// </summary>
+	public abstract partial class CallsignRegistry : global::Goedel.Protocol.JsonObject {
+
+		/// <summary>
+        /// Tag identifying this class
+        /// </summary>
+		public override string _Tag =>__Tag;
+
+		/// <summary>
+        /// Tag identifying this class
+        /// </summary>
+		public new const string __Tag = "CallsignRegistry";
+
+		/// <summary>
+        /// Dictionary mapping tags to factory methods
+        /// </summary>
+		public static Dictionary<string, JsonFactoryDelegate> _TagDictionary=> _tagDictionary;
+		static Dictionary<string, JsonFactoryDelegate> _tagDictionary = 
+				new Dictionary<string, JsonFactoryDelegate> () {
+			};
+
+        [ModuleInitializer]
+        internal static void _Initialize() => AddDictionary(ref _tagDictionary);
+
+
+		/// <summary>
+        /// Construct an instance from the specified tagged JsonReader stream.
+        /// </summary>
+        /// <param name="jsonReader">Input stream</param>
+        /// <param name="result">The created object</param>
+        public static void Deserialize(JsonReader jsonReader, out JsonObject result) => 
+			result = jsonReader.ReadTaggedObject(_TagDictionary);
+
+		}
+
+
+
+		// Service Dispatch Classes
+
+
+
+		// Transaction Classes
+	}
+
